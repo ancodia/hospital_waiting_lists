e42414ca1e9112aa2ea3d706bb697c41a43cf4aa
Danny Regan <danny.regan@protonmail.com>
2020-04-25 16:56:42 +0100

    updates to data transformation code

--- analysis/data_transformation.R
+++ analysis/data_transformation.R
@@ -1,39 +1,86 @@
 source("analysis/helper.R")
+#######################################
+# Data transformation 
+# - load and process source data, 
+#   then output to new csv files
+#######################################
 
-# column names to avoid duplicates, 
+# column names vectors to avoid duplicates on load of data, 
 # some source files have a space in column names, while others use "_".
 # Using underscore for all
-columns1 <- c("Archive_Date",
-              "Hospital_Group",
-              "Hospital_HIPE",
-              "Hospital_Name",
-              "Specialty_HIPE",
-              "Specialty_Name",
-              "Case_Type",
-              "Adult_Child",
-              "Age_Profile",
-              "Time_Bands",
-              "Total")
+
+#######################################
+# inpatient day case data columns
+#######################################
+# names:
+columns_ipdc <- c("Archive_Date",
+                  "Hospital_Group",
+                  "Hospital_HIPE",
+                  "Hospital_Name",
+                  "Specialty_HIPE",
+                  "Specialty_Name",
+                  "Case_Type",
+                  "Adult_Child",
+                  "Age_Profile",
+                  "Time_Bands",
+                  "Total")
+# types
+# https://www.rdocumentation.org/packages/vroom/versions/1.0.2/topics/vroom
+# c = character, i = integer, n = number, d = double, l = logical, f = factor, 
+# D = date, T = date time, t = time
+column_types_ipdc <- "cfffffffffi"
+
+#######################################
+# outpatient data columns
+#######################################
+# names:
 # no case type in outpatients waiting list data
-columns2 <- columns1[columns1 != "Case_Type"]
+columns_op <- columns_ipdc[columns_ipdc != "Case_Type"]
+
+# types:
+column_types_op <- "cffffffffi"
 
 # load data from csv files
 # inpatient/day case waiting lists
-ipdc_data <- combine_csv_data("datasets/ipdc", column_names = columns1)
+ipdc_data <- combine_csv_data("datasets/ipdc", 
+                              column_names = columns_ipdc,
+                              column_types = column_types_ipdc)
 nrow(ipdc_data)
 # inpatient/day case waiting lists for GI Endoscopy patients
-ipdc_gi_data <- combine_csv_data("datasets/ipdc_gi", column_names = columns1)
+ipdc_gi_data <- combine_csv_data("datasets/ipdc_gi", 
+                                 column_names = columns_ipdc,
+                                 column_types = column_types_ipdc)
 nrow(ipdc_gi_data)
 # out patient waiting lists
-op_data <- combine_csv_data("datasets/op", column_names = columns2)
+op_data <- combine_csv_data("datasets/op", 
+                            column_names = columns_op,
+                            column_types = column_types_op)
 nrow(op_data)
 
-# inspect dataframes
+# inspect tibble structures
 str(ipdc_data)
 str(ipdc_gi_data)
 str(op_data)
 
-# there is differences in column naming conventions in each of the frames, 
-# "." vs "_" for spaces. This needs to be updated so that all use an underscore
-ipdc_data <- replace_character_in_column_names(ipdc_data, ".", "_")
+# dates were imported as characters because there is a mixture of Y-m-d and d/m/Y formats
+# so they need to be converted to a single date type
+ipdc_data <- convert_dates(ipdc_data)
+ipdc_gi_data <- convert_dates(ipdc_gi_data)
+op_data <- convert_dates(op_data)
 
+# check that Archive_Date is now date type
+str(ipdc_data)
+str(ipdc_gi_data)
+str(op_data)
+
+# removing Speciality_HIPE column because it doesn't provide any useful information
+# dplyr select
+ipdc_data <- select(ipdc_data, -c(Specialty_HIPE))
+ipdc_gi_data <- select(ipdc_gi_data, -c(Specialty_HIPE))
+op_data <- select(op_data, -c(Specialty_HIPE))
+
+# Hospital_HIPE code is useful in the case where a hospital's name has changed
+# for example Letterkenny General Hospital has been renamed to 
+# Letterkenny University Hospital. There is an issue with these Hospital_HIPE though
+# where some have a proceeding 0 such as Our Lady's Childeren's Hospital Crumlin
+# having 0941 as HIPE in some records while it is 941 in other.
--- analysis/helper.R
+++ analysis/helper.R
@@ -1,20 +1,22 @@
 library(tidyverse)
 library(vroom)
+library(lubridate)
 
-combine_csv_data <- function(path_to_files, column_names){
+combine_csv_data <- function(path_to_files, column_names, column_types){
   # list of csv files
   csv_files <- dir(path = path_to_files, 
                    pattern=".*[.]csv", full.names = TRUE)
-  # use vroom library to combine files
-  combined_data <- vroom(csv_files, skip = 1, col_names = column_names)
+  # use vroom library to combine files into single tibble
+  combined_data <- vroom(csv_files, skip = 1, 
+                         col_names = column_names, 
+                         col_types = column_types)
   return(combined_data)
 }
 
-replace_character_in_column_names <- function(dataframe, old, new){
-  # replace "old" with "new" character in all columns of supplied dataframe
-  colnames(dataframe) <- gsub(old, new, colnames(dataframe), fixed = TRUE)
-  
-  # merge any duplicate columns
-  
-  return(dataframe)
+convert_dates <- function(data){
+  # parse_date_time() function from lubridate package
+  data$Archive_Date <- parse_date_time(x = data$Archive_Date,
+                                       orders = c("Y-m-d", "d/m/Y"))
+  data$Archive_Date <- as.Date(data$Archive_Date)
+  return(data)
 }
