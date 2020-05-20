a6cfbba1fe666a94030cc2a77d120b15b83ed68e
Danny Regan <danny.regan@protonmail.com>
2020-05-20 19:41:31 +0100

    updates to predictions

--- helpers/predictions_helper.R
+++ helpers/predictions_helper.R
@@ -1,2 +1,16 @@
 source("helpers/load_libs.R")
 source("helpers/descriptive_statistics_helper.R")
+
+plot_arima_model <- function(model_forecast, train_data = NULL, test_data = NULL){
+  clrs <- c("black", "blue", "red", "purple")
+  
+  model_forecast %>% autoplot(xlab = "Year", 
+                              ylab = "Patients waiting") +
+    autolayer(model_forecast$mean, series="Forecast") +
+    {if (!is.null(train_data) && (!is.null(test_data)))
+      autolayer(fitted(model_forecast), series='Fitted')} + 
+    {if (!is.null(train_data)) autolayer(train_data, series = 'Train')} +
+    {if (!is.null(test_data)) autolayer(test_data, series='Test')} +
+    guides(colour=guide_legend(title="Data series")) +
+    scale_color_manual(values=clrs)
+}
--- predictive_modelling/time_series_predictions.R
+++ predictive_modelling/time_series_predictions.R
@@ -20,6 +20,25 @@ nrow(saolta)
 saolta_ts <- ts(saolta$Total, frequency = 12, start = c(2014, 1))
 saolta_ts
 
+# check if any na records exist in time series
+anyNA(saolta_ts)
+
+# Show time series data
+plot(saolta_ts,
+     xlab="Year", 
+     ylab = "Patients waiting",
+     main="Saolta University Hospital Group Waiting Lists 2014-19")
+# linear relationship between number of patients waiting and time
+abline(reg = lm(saolta_ts ~ time(saolta_ts)))
+
+# use boxplot to check seasonality
+boxplot(saolta_ts ~ cycle(saolta_ts),
+        xlab="Year", 
+        ylab = "Patients waiting",
+        main ="Saolta University Hospital Group Waiting Lists 2014-19")
+# doesn't appear to be seasonal, mean is fairly stable over the course of years
+
+
 # for ARIMA the following values are required, ARIMA(p, d, q):
 # p: the number of autoregressive terms (AR order)
 # d: the number of nonseasonal differences needed for stationarity
@@ -35,7 +54,7 @@ saolta_ts_decomposed <- plot_timeseries_data(saolta_ts, title = "Saolta")
 # decomposed time series plot looks like there is seasonality, 
 # checking variance of decomposed series with original
 apply(saolta_ts_decomposed$time.series, 2, var) / var(saolta_ts)
-# the trend explains almost all the variance in the time series
+# the trend explains almost all (97.5%) the variance in the time series
 
 # confirm with isSeasonal test
 seastests::isSeasonal(saolta_ts)
@@ -54,7 +73,7 @@ ndiffs(saolta_ts_diff) # = 0
 plot(saolta_ts_diff, type = "l", main = "Differenced Saolta Time Series")
 
 astsa::acf2(saolta_ts_diff, main = "Saolta Waiting Lists Time Series ACF/PACF")
-# now the acf/pacf drops below the dotted line 
+# now the acf/pacf quickly drops below the dotted line 
 # indicating that the majority of values are not significantly different from 0
 
 # Test for stationarity
@@ -77,38 +96,47 @@ tseries::kpss.test(saolta_ts_diff)
 # both cut off after lag 1 so parameters for ARMA(p,q) = (1,0), (0,1) or (1,1)
 # => 3 ARIMA models can be proposed: ARIMA(1, 1, 0), ARIMA(0, 1, 1) and ARIMA(1, 1, 1)
 
-# split time series into train and test sets (80:20 split) 
-test_size <- round(length(saolta_ts) * 0.2)
-split_saolta_ts <- ts_split(saolta_ts, sample.out = test_size)
-train <- split_saolta_ts$train
-test <- split_saolta_ts$test
+# split time series into train and test
+# training from 2014-18 data
+train <- window(x = saolta_ts, start = c(2014, 1), end = c(2018, 12))
+# test with 2019 data
+test <- window(x = saolta_ts, start = c(2019, 1), end = c(2019, 12))
 
-# build the 3 models
+### BUILD
+# build the 3 models and an auto arima model
 arima_model1 = forecast::Arima(train, order = c(1, 1, 0))
-arima_model1
-accuracy(arima_model1)
-
 arima_model2 = forecast::Arima(train, order = c(0, 1, 1))
-arima_model2
-accuracy(arima_model2)
-
 arima_model3 = forecast::Arima(train, order = c(1, 1, 1))
+auto_arima_model <- auto.arima(train) # gives ARIMA(1,1,0) with drift 
+# drift is the amount of change over time - uses the average change seen in historical data
+
+### EVALUATE
+arima_model1
+# AIC: 1032.69
+model1_accuracy <- accuracy(arima_model1)
+str(model1_accuracy)
+# MAPE: 1.093352
+
+arima_model2
+# AIC: 1036.51
+model2_accuracy <- accuracy(arima_model2)
+# MAPE: 1.135171
+
 arima_model3
-accuracy(arima_model3)
+# AIC: 1034.2
+model3_accuracy <- accuracy(arima_model3)
+# MAPE: 1.0715
 
-# AIC:
-#   ARIMA(1, 1, 0) = 999.51
-#   ARIMA(0, 1, 1) = 1003.37
-#   ARIMA(1, 1, 1) = 1001.01
+auto_arima_model
+# AIC: 1031.2
+model_auto_accuracy <- accuracy(auto_arima_model)
+# MAPE: 1.070919
 
-# MAPE:
-#   ARIMA(1, 1, 0) = 1.115
-#   ARIMA(0, 1, 1) = 1.166
-#   ARIMA(1, 1, 1) = 1.09
 
-# check quantile-quantile plots for residuals of each model to determine if they are normally distributed
+# check quantile-quantile plots and histogram for residuals of each model to determine 
+# if they are normally distributed and then  verify with shapiro-wilk test
 opar <- par(no.readonly = TRUE)
-par(mfrow = c(2, 3))
+par(mfrow = c(2, 2))
 
 qqnorm(arima_model1$residuals, main = "Q-Q Plot: ARIMA(1,1,0)")
 qqline(arima_model1$residuals)
@@ -119,52 +147,119 @@ qqline(arima_model2$residuals)
 qqnorm(arima_model3$residuals, main = "Q-Q Plot: ARIMA(1,1,1)")
 qqline(arima_model3$residuals)
 
-hist(arima_model1$residuals, main = "Histogram: ARIMA(1,1,0)", 
-     xlab = "Residuals", 
-     sub = paste("Shapiro p-value:", 
-                 round(shapiro.test(arima_model1$residuals)$p.value, 6)))
-hist(arima_model2$residuals, main = "Histogram: ARIMA(0,1,1)", 
-     xlab = "Residuals",
-     sub = paste("Shapiro p-value:", 
-                 round(shapiro.test(arima_model2$residuals)$p.value, 6)))
-hist(arima_model3$residuals, main = "Histogram: ARIMA(1,1,1)", 
-     xlab = "Residuals",
-     sub = paste("Shapiro p-value:", 
-                 round(shapiro.test(arima_model3$residuals)$p.value, 6)))
+qqnorm(auto_arima_model$residuals, main = "Q-Q Plot: (Auto) ARIMA(1,1,0) with drift")
+qqline(auto_arima_model$residuals)
+par(opar)
 
+opar <- par(no.readonly = TRUE)
+par(mfrow = c(2, 2))
+hist(arima_model1$residuals, main = "Histogram: ARIMA(1,1,0)", 
+     xlab = "Residuals")
+model1_shapiro <- shapiro.test(arima_model1$residuals)
+
+hist(arima_model2$residuals, main = "Histogram: ARIMA(0,1,1)", 
+     xlab = "Residuals")
+model2_shapiro <- shapiro.test(arima_model2$residuals)
+
+hist(arima_model3$residuals, main = "Histogram: ARIMA(1,1,1)", 
+     xlab = "Residuals")
+model3_shapiro <- shapiro.test(arima_model3$residuals)
+
+hist(auto_arima_model$residuals, main = "Histogram: (Auto) ARIMA(1,1,0) with drift", 
+     xlab = "Residuals")
+model_auto_shapiro <- shapiro.test(auto_arima_model$residuals)
 par(opar)
 
 # Use Ljung-Box test
 # H0 = the autocorrelations are all zero
-Box.test(arima_model1$residuals, type = "Ljung-Box")
-Box.test(arima_model2$residuals, type = "Ljung-Box")
-Box.test(arima_model3$residuals, type = "Ljung-Box")
+model1_ljung <- Box.test(arima_model1$residuals, type = "Ljung-Box")
+model2_ljung <- Box.test(arima_model2$residuals, type = "Ljung-Box")
+model3_ljung <- Box.test(arima_model3$residuals, type = "Ljung-Box")
+model_auto_ljung <- Box.test(auto_arima_model$residuals, type = "Ljung-Box")
 
-# Box-Ljung test p-values:
-#   ARIMA(1, 1, 0) = 0.1811
-#   ARIMA(0, 1, 1) = 0.3488
-#   ARIMA(1, 1, 1) = 0.4944
 
-# from evaluating the models, model3 appears to be the best fit
+# create dataframe with evaluation metrics
+model_ids <- c("ARIMA(1,1,0)", "ARIMA(0,1,1)", "ARIMA(1,1,1)", "(Auto) ARIMA(1,1,0) w/ drift")
+aic_valules <- c(arima_model1$aic, arima_model2$aic, arima_model3$aic, auto_arima_model$aic)
+mape_values <- c(model1_accuracy[, "MAPE"],
+                 model2_accuracy[, "MAPE"],
+                 model3_accuracy[, "MAPE"],
+                 model_auto_accuracy[, "MAPE"])
+shapiro_pvalues <- c(round(model1_shapiro$p.value, 5), 
+                     round(model2_shapiro$p.value, 5), 
+                     round(model3_shapiro$p.value, 5), 
+                     round(model_auto_shapiro$p.value, 5))
+ljung_pvalues <- c(round(model1_ljung$p.value, 5), 
+                   round(model2_ljung$p.value, 5), 
+                   round(model3_ljung$p.value, 5), 
+                   round(model_auto_ljung$p.value, 5))
 
+evaluation_df <- data.frame(model_ids, aic_valules, mape_values, shapiro_pvalues, ljung_pvalues)
+colnames(evaluation_df) <- c("Model", "AIC", "MAPE", "Shapiro-Wilk", "Ljung-box")
+evaluation_df
+
+### VALIDATION
+# check model predictions
 # plot forecasts for each model
-opar <- par(no.readonly = TRUE)
-par(mfrow = c(1, 3))
 
-forecast_model1 <- forecast(arima_model1, test_size)
-plot(forecast_model1)
+forecast_model1 <- forecast(arima_model1, h = 12)
+forecast_model1
+plot_arima_model(forecast_model1, train, test)
 
-forecast_model2 <- forecast(arima_model2, test_size)
-plot(forecast_model2)
 
-forecast_model3 <- forecast(arima_model3, test_size)
-plot(forecast_model3)
+forecast_model2 <- forecast(arima_model2, h = 12)
+forecast_model2
+plot_arima_model(forecast_model2, train, test)
 
-par(opar)
+forecast_model3 <- forecast(arima_model3, h = 12)
+forecast_model3
+plot_arima_model(forecast_model3, train, test)
 
-# check what model is suggested with auto arima
-fit <- auto.arima(saolta_ts)
-fit
+auto_forecast <- forecast(auto_arima_model, h = 12)
+auto_forecast
+plot_arima_model(auto_forecast, train, test)
+
+test
+# make actuals_predicted dataframe
+# for each arima model
+actuals_predictions <- data.frame(cbind(cycle(test), 
+                                        test, 
+                                        forecast_model1$mean, 
+                                        forecast_model2$mean, 
+                                        forecast_model3$mean, 
+                                        auto_forecast$mean))
+colnames(actuals_predictions) <- c("2019", 
+                                  "Actual", 
+                                  "ARIMA(1,1,0)", 
+                                  "ARIMA(0,1,1)", 
+                                  "ARIMA(0,1,1)", 
+                                  "(Auto) ARIMA(1,1,0) w/ drift")
+actuals_predictions
+
+
+correlation_accuracy <- cor(actuals_predictions[-1])
+correlation_accuracy
+
 ######################
+### FORECASTING
+# build model using parameters suggested by auto arima
+model <- auto.arima(saolta_ts)
+model
+
+forecast <- forecast(model, h = 12)
+forecast
+
+plot_arima_model(forecast)
+
+# check forecasted increase compared to 2019
+actuals_predictions_2020 <- data.frame(as.vector(test), as.vector(forecast$mean))
 
 
+percent_change <- ((actuals_predictions_2020[,2] - actuals_predictions_2020[,1]) / 
+                     actuals_predictions_2020[,1]) * 100 
+
+actuals_predictions_2020$pct_change <- percent_change
+colnames(actuals_predictions_2020) <- c("2019 (Actual)", "2020 (Forecasted)", "Percentage Change")
+actuals_predictions_2020
+
+mean(actuals_predictions_2020[,3])
